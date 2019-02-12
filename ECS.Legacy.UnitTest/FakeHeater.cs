b36e1752354f4746babbf842773f4e953becf03b
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2019-02-12 13:52:19 +0100

    Ændringer af solutionavn grundet konflikter med Jenkins

--- ECS.Legacy.UnitTest/FakeHeater.cs
+++ ECS.Legacy.UnitTest/FakeHeater.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using ECS.Legacy;
 
 namespace ECS.Legacy.UnitTest
 {
-    class FakeHeater
+    class FakeHeater : IHeater
     {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
     }
 }
